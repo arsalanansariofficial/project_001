03872b9cc18a36023a10e5153dece58b5ef85416
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-02-22 16:22:04 +0530

    update user repository

--- app/_repository/user-repository.ts
+++ app/_repository/user-repository.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import crypto, { sign } from 'node:crypto';
+import crypto from 'node:crypto';
 
 import { prisma } from '../_db/db';
 
@@ -15,24 +15,16 @@ function hashPassword(salt: string, password: string) {
   return crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
 }
 
-function verifyToken(token: string, signature: string) {
-  try {
-    return jwt.verify(token, signature);
-  } catch (e) {
-    return null;
-  }
-}
-
 async function createToken(userId: string, signature: string) {
   const token = await prisma.token.create({
-    data: { userId, token: jwt.sign({ id: userId }, signature, { expiresIn }) }
+    data: { userId, token: jwt.sign({ userId }, signature, { expiresIn }) }
   });
 
   setTimeout(async () => {
     await prisma.token.delete({ where: { id: token.id } });
   }, expiresIn);
 
-  return { token: jwt.sign({ id: userId }, signature, { expiresIn }) };
+  return { token: token.token };
 }
 
 export async function singupUser(
