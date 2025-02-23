d0ee63458e25bb6ba24aa0327eb62e49a32c5133
Arsalan Ansari <arsalanansariofficial@outlook.com>
2025-02-23 13:53:43 +0530

    update server.ts

--- app/server.ts
+++ app/server.ts
@@ -1,21 +1,36 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import { Express } from 'express-serve-static-core';
 
+import * as lib from './_lib/const';
 import urlRouter from './_router/url-router';
 import userRouter from './_router/user-router';
 import notFound from './_middleware/not-found-handler';
 
-const PORT = process.env.PORT || 8080;
-const app = express();
+function addMiddlewares(app: Express) {
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(express.urlencoded({ extended: false }));
+}
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.urlencoded({ extended: false }));
+function addRouteHandlers(app: Express) {
+  app.use(urlRouter);
+  app.use(userRouter);
+  app.use(notFound);
+}
 
-app.use(urlRouter);
-app.use(userRouter);
-app.use(notFound);
+function serve() {
+  if (!lib.port) throw new Error('Port should be valid');
+  if (!lib.salt) throw new Error('Salt should be valid');
 
-app.listen(PORT, function () {
-  console.log(`Application listening on PORT ${PORT}`);
-});
+  if (!lib.signature) throw new Error('Signature should be valid');
+  if (!lib.expiresIn) throw new Error('Expires In should be valid');
+  if (!lib.databaseUrl) throw new Error('Database URL should be valid');
+
+  const app = express();
+  addMiddlewares(app);
+  addRouteHandlers(app);
+  app.listen(lib.port);
+}
+
+serve();
